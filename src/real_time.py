8a584853140e4342d13d32ca55ade56f2093b6c5
nourhanezz1112004 <norhanezzalarab2021@gmail.com>
2025-12-21 04:09:59 +0200

    Update real_time.py

--- src/real_time.py
+++ src/real_time.py
@@ -10,21 +10,32 @@ class CameraWorker(threading.Thread):
         self.cap = cv2.VideoCapture(self.src)
         self.running = False
         self.frame = None
-        self.pred = ('', 0.0)
+        self.pred = ("", 0.0)
+
+        self.frame_count = 0          
+        self.predict_every = 5        #  كل كام فريم نعمل predict جديد
+
 
     def run(self):
-        self.running = True
-        while self.running:
-            ret, frame = self.cap.read()
-            if not ret:
-                break
-            self.frame = frame
+    self.running = True
+    while self.running:
+        ret, frame = self.cap.read()
+        if not ret:
+            break
+
+        self.frame = frame
+        self.frame_count += 1
+
+        # 🔥 Prediction كل N frames فقط
+        if self.frame_count % self.predict_every == 0:
             try:
                 label, prob = inference.predict(frame)
                 self.pred = (label, prob)
             except Exception:
-                self.pred = ('Model not loaded', 0.0)
-        self.cap.release()
+                self.pred = ("Model not loaded", 0.0)
+
+    self.cap.release()
+
 
     def stop(self):
         self.running = False
